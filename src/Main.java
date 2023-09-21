f5f3268fe5b7b4faa81211a2deafc7f6a5579c42
Pedro Rocha <pedro.leonardo.pereira.rocha@gmail.com>
2023-09-21 10:55:39 -0300

    Entrada de nome e CPF (falta validação)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
      // Classe  Objeto           Construtor
         Turista mochileiro = new Turista(); // Padrão
-
         // Definir formato do objeto
         mochileiro.setNome("Lindsay Lohan");
-        mochileiro.setCpf(Validacao.cpf("123"));
+        mochileiro.setCpf("123");
         Turista mochileira = new Turista("Agnes"); // Personalizado
         // Definir comportamento
         String retorno = mochileira.viajar();
         System.out.println(retorno);
+
+        // Criar um novo objeto
+        Turista outroTurista = new Turista();
+        String nome = JOptionPane.showInputDialog("Entre com seu nome");
+        outroTurista.setNome(nome);
+        JOptionPane.showMessageDialog(null, "O nome digitado foi "+
+                outroTurista.getNome());
+        // Criar a captura do cpf deste objeto
+        String cpf = JOptionPane.showInputDialog("Entre com o seu CPF");
+        if(Validacao.cpf(cpf))
+            outroTurista.setCpf(cpf);
+        else
+            System.out.println("ERRO! CPF NÃO VALIDADO!!!");
+        JOptionPane.showMessageDialog(null, outroTurista.getCpf()
+                + " validado!");
 
     }
 }
--- src/Validacao.java
+++ src/Validacao.java
@@ -1,5 +1,6 @@
 public class Validacao {
-    public static String cpf(String _cpf){
-        return _cpf;
+    public static boolean cpf(String _cpf){
+        // TODO - Aplicar a validação de cpf
+        return true;
     }
 }
